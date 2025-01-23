f2c54dfcb6b0e21356fdde3cfb5dcbb8a26693b8
oindrilabanerjee <banerjee.oindrila.ob@gmail.com>
2025-01-24 01:46:50 +0530

    mods

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center min-h-screen p-8 pb-20 gap-04 sm:p-20">
-      <div className="bh-10 w-10 tracking-wider"> Oindrila_
-
+    <div className="grid grid-rows-[20px_1fr_20px] items-center min-h-screen p-8 pb-20 gap-04 sm:p-16 tracking-wider">
+      <div className="h-10 w-10"> Oindrila_
       </div>
-      
+      <div className="bw-full sm:w-1/2 md:w-1/3 h-64 sm:h-72 md:h-80 items-center ">
+            <p className="sm:text-5xl md:text-6xl lg:text-8xl pb-24 "> OINDRILA__</p>
+            <p className="sm:text-5xl md:text-6xl lg:text-8xl pb-24 "> __BANERJEE</p>
+      </div>
     </div>
   );
 }
